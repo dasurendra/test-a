9fa606264eea64c7b4cbbd35e67590b308657dfc
Your Name <dsurendra@gmil.com>
2024-11-18 13:20:08 +1100

    added chart.js

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
   return (
     <Router>
       <Routes>
+        {" "}
+        <Route path="/" element={<SignIn />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
